f022fd12b7c62071460d8690ba89df572a6ca265
Andy Nguyen <anguyen1119@gmail.com>
2019-10-30 13:17:00 -0400

    Add store to router

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,6 +4,10 @@ import { routerReducer } from 'react-router-redux';
 import posts from './posts';
 import comments from './comments';
 
-const rootReducer = combineReducers({ posts, comments, router: routerReducer });
+const rootReducer = combineReducers({
+  posts,
+  comments,
+  routing: routerReducer
+});
 
 export default rootReducer;
