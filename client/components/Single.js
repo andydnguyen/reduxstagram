f15d77cc95f0db6d2afbdbed7b5a8b33b45634b9
Andy Nguyen <anguyen1119@gmail.com>
2019-11-02 15:20:03 -0400

    Add single page photo

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
-    return <div className='single-photo'>Single</div>;
+    const { postId } = this.props.params;
+    // index of the post
+    const i = this.props.posts.findIndex(post => post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
+    // get us the post
+    return (
+      <div className='single-photo'>
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
+      </div>
+    );
   }
 });
 
